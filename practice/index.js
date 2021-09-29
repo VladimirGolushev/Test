6c0b33ed0176ec1bc4d012f148e963f5cc36d6fc
VladimirGolushev <wanted6576@gmail.com>
2021-09-29 22:04:28 +0300

    second day of training

--- practice/index.js
+++ practice/index.js
@@ -1,1 +1,19 @@
- const modal = $.modal()
+ const modal = $.modal({
+     title: 'Vladimir Modal',
+     closable: true,
+     content: `
+     <p>Lorem ipsum dolor sit.</p>     
+     <p>Lorem ipsum dolor sit.</p>
+    `,
+    width: '300px',
+    footerButtons: [
+        {text: 'Ok', type: 'primary', headler(){
+            console.log('Primary btn click')
+            modal.close()
+        }},
+        {text: 'Cancel', type: 'danger', headler(){
+            console.log('Danger btn click')
+            modal.close()
+        }},       
+    ] 
+ })
